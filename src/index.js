442640029a81bdaee2af165aafb064abec9ac392
Marc Toensing <marc.toensing@gmail.com>
2021-11-03 20:57:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,10 +39,11 @@ registerBlockType('yarpp-block/list', {
             
             <SelectControl
 							label={ __('YARPP block type', 'yarpp_block') }
+              help= { __('Displays 3 posts of the configured type.', 'yarpp_block') }
 							value={ props.attributes.blocktype }
 							options={ [
-								{ value: 'related', label: 'Related' },
-								{ value: 'latest', label: 'Latest' },
+								{ value: 'related', label: __('Related posts (default)', 'yarpp_block') },
+								{ value: 'latest', label: __('Latest posts excluding related posts', 'yarpp_block') },
 							] }
 							onChange={ ( blocktype ) => props.setAttributes( { blocktype: blocktype } ) }
 						/>
